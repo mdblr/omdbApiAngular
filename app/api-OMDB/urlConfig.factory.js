742bd1cd52ec5e9be342495f88c6029a46b325fd
Max Dobler <mdblr@outlook.com>
2016-05-13 11:54:49 -0700

    refactor into new service from omdb get ajax service

---
+++ app/api-OMDB/urlConfig.factory.js
@@ -0,0 +1,42 @@
+(function() {
+  angular
+    .module('omdbApp')
+    .factory('urlConfig', urlConfig)
+
+    function urlConfig() {
+
+      var url = {};
+      var all, title, media
+
+      url.base = 'http://www.omdbapi.com/?';
+
+      // api query param for general search
+      all = 's='
+      title = 't='
+      media = '&type='
+
+      url.config = function(input, search, mediaType) {
+
+        input = input.trim()
+        input = input.replace(' ', '+')
+
+        search === 'title' ? input = (title + input) : input = (all + input)
+
+        if (mediaType === 'movie') {
+          input = input + media.concat('movie')
+        }
+        else if (mediaType === 'episode') {
+          input = input + media.concat('episode')
+        }
+        else if (mediaType === 'series') {
+          input = input + media.concat('series')
+        }
+
+        url.ready = url.base.concat(input)
+
+        return url.ready
+      }
+
+      return url
+    }
+})();
