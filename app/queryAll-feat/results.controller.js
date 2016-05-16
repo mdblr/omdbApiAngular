efc3389f7b7a396ae4068f73f9c90c86f2950757
Max Dobler <mdblr@outlook.com>
2016-05-15 19:44:05 -0700

    catagorize data in typeOfResult object

--- app/queryAll-feat/results.controller.js
+++ app/queryAll-feat/results.controller.js
@@ -8,15 +8,29 @@
 
     function Search(searchOMDB, $routeParams) {
 
-      let vm = this;
+      var vm = this;
       let userInput = $routeParams.movieString;
       let media = $routeParams.media;
+      vm.typeOfResult = {movies: [] , episodes: [], series: []};
 
       vm.films, vm.error;
+      vm.filter = 'all';
 
       searchOMDB.titlesLike(userInput, 'search', media)
       .then(function(data) {
-        !data ? vm.nope = "Sorry, we don't know that one" : vm.films = data;
+        !data ? vm.nope = "Sorry, we don't know that one" : vm.films = data ;
+        for (var i = 0; i < 10; i ++) {
+          vm.films[i].metaFilter = 'all';
+          if (vm.films[i].Type === 'movie') {
+            vm.typeOfResult.movies.push(vm.films[i])
+          }
+          else if (vm.films[i].Type === 'episode') {
+            vm.typeOfResult.episode.push(vm.films[i])
+          }
+          else if (vm.films[i].Type === 'series') {
+            vm.typeOfResult.series.push(vm.films[i])
+          }
+        }
       })
       .catch(function(error) {
         vm.error = "Something wen't wrong! Sorry. "
